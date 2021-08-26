9f5658308e0a92abd1ee07140e992deee848daf9
Abraham Isaias <A00835121@tec.mx>
2021-08-26 09:53:20 -0500

    Trabajos finales

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -2,6 +2,10 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    def volumen_prisma(a,b,p):
+        return a*b*p
+
+
     b = float(input("Dame la base: "))
     a = float(input("Dame la altura: "))
     p = float(input("Dame la profundidad: "))
